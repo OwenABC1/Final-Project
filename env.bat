b1dc9f37e1851c882b4160016c887f6847c27bf0
Romulus0805 <emb@icloud.com>
2025-01-08 13:23:37 -0800

    add button format

---
+++ env.bat
@@ -0,0 +1,5 @@
+set MONGO_CONNECTION_STRING=mongodb+srv://evanderbg2:LetipDKKn1EqVUTn@cluster0.xaaeb.mongodb.net/?retryWrites=true
+set MONGO_DBNAME=ForumProDB
+set SECRET_KEY=asdf
+set GITHUB_CLIENT_ID=Ov23liTN9mAulkWWld4H
+set GITHUB_CLIENT_SECRET=149ef47b78dec273540d01a76cf64cd307483e44
