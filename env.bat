18b1d0bbe2848adf179052955809c3d303199f0b
EvanderGibson <evanderbg2@gmail.com>
2025-01-21 13:51:32 -0800

    hit buttooon

--- env.bat
+++ env.bat
@@ -1,7 +1,5 @@
-set MONGO_CONNECTION_STRING=mongodb+srv://evanderbg2:CdWGDhkx8pgpMcTa@cluster0.oppsq.mongodb.net/?retryWrites=true
+set MONGO_CONNECTION_STRING=mongodb+srv://evanderbg2:LmBFV2qzLPuw6q9z@cluster0.oppsq.mongodb.net/?retryWrites=true
 set MONGO_DBNAME=FinalProjectDB
 set SECRET_KEY=asdf
 set GITHUB_CLIENT_ID=Ov23liTN9mAulkWWld4H
 set GITHUB_CLIENT_SECRET=149ef47b78dec273540d01a76cf64cd307483e44
-
-
