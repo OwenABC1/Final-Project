454159df195944473fb1072b3a1ce7d067180b8c
Romulus0805 <emb@icloud.com>
2025-01-21 13:52:25 -0800

    fixed env

--- env.bat
+++ env.bat
@@ -1,4 +1,4 @@
-set MONGO_CONNECTION_STRING=mongodb+srv://evanderbg2:CdWGDhkx8pgpMcTa@cluster0.oppsq.mongodb.net/?retryWrites=true
+set MONGO_CONNECTION_STRING=mongodb+srv://evanderbg2:LmBFV2qzLPuw6q9z@cluster0.oppsq.mongodb.net/?retryWrites=true
 set MONGO_DBNAME=FinalProjectDB
 set SECRET_KEY=asdf
 set GITHUB_CLIENT_ID=Ov23liTN9mAulkWWld4H
